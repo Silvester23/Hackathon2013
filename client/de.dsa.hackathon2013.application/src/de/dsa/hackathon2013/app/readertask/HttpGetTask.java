37f53550b7f73191baebe659877a3b9e931284b8
Jan Murmann <jan@23x5.net>
2013-11-19 00:54:01 +0100

    added HttpGetTask.java

---
+++ client/de.dsa.hackathon2013.application/src/de/dsa/hackathon2013/app/readertask/HttpGetTask.java
@@ -0,0 +1,61 @@
+package de.dsa.hackathon2013.app.readertask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import de.dsa.hackathon2013.app.DSADiagnosticsActivity;
+
+public class HttpGetTask extends AsyncTask<String, String, String>{
+	DSADiagnosticsActivity callback;
+	
+	public HttpGetTask(DSADiagnosticsActivity activity) {
+		callback = activity;
+	}
+
+    @Override
+    protected String doInBackground(String... uri) {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response;
+        String responseString = null;
+        try {
+            response = httpclient.execute(new HttpGet(uri[0]));
+            StatusLine statusLine = response.getStatusLine();
+            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                response.getEntity().writeTo(out);
+                responseString = out.toString();
+                out.close();
+            } else{
+                //Closes the connection.
+                response.getEntity().getContent().close();
+                throw new IOException(statusLine.getReasonPhrase());
+            }
+        } catch (ClientProtocolException e) {
+            //TODO Handle problems..
+        } catch (IOException e) {
+            //TODO Handle problems..
+        }
+        return responseString;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        
+        callback.onResponse(result);
+        
+        
+        
+    }
+}
